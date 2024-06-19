b80c6dc4bc3977572a537a1f0380f79abff15553
Dangcaominh <minh.dangrandomname@hcmut.edu.vn>
2024-06-19 09:29:19 +0700

    ModifiedADCRead

--- ADC_Calibration/linear_regression.py
+++ ADC_Calibration/linear_regression.py
@@ -9,32 +9,12 @@ plt.title('Response of ESP32-S ADC')
 plt.xlabel('ADC Sample Mean')
 plt.ylabel('Voltage')
 
-#-------------------------------------------------------------------------------------
-#-----------------LINEAR REGRESSION FOR VOLTAGE in RANGE 0.2 -> 0.24------------------
-#-------------------------------------------------------------------------------------
-
-filtered_data_x = x_axis[10:15]
-filtered_data_y = y_axis[10:15]
+filtered_data_x = x_axis[10:37]
+filtered_data_y = y_axis[10:37]
 slope, intercept, r, p, std_err = stats.linregress(filtered_data_x, filtered_data_y)
 def linear_regression(x):
   return slope * x + intercept
 print(f"Linear Regression Function y = {slope}* x + {intercept}")
 regression_model = list(map(linear_regression, filtered_data_x))
 plt.plot(filtered_data_x, regression_model, 2, "blue")
-
-#-------------------------------------------------------------------------------------
-#-----------------LINEAR REGRESSION FOR VOLTAGE in RANGE 0.24 -> 2.4------------------
-#-------------------------------------------------------------------------------------
-
-filtered_data_x = x_axis[15:37]
-filtered_data_y = y_axis[15:37]
-slope, intercept, r, p, std_err = stats.linregress(filtered_data_x, filtered_data_y)
-def linear_regression(x):
-  return slope * x + intercept
-print(f"Linear Regression Function y = {slope}* x + {intercept}")
-regression_model = list(map(linear_regression, filtered_data_x))
-plt.plot(filtered_data_x, regression_model, 2, "green")
-plt.show()
-
-
-# i = 10 -> 36
+plt.show();
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,171 @@
 #include <Arduino.h>
-#include "esp_adc_cal.h"
+#include <Adafruit_ADS1X15.h>
+#include <SPI.h>
+#include <AiEsp32RotaryEncoder.h>
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+#include <IRremote.h>
+#include <RTClib.h>
 
-const double GAIN = 7.889112903225806451612903225806451612903225806451;
-double ModifiedAnalogReadVolts(uint8_t pin, uint16_t tolerance = 10);
-inline double linear_regression(double x);
-#define ADC_PIN 36
-int DACData = 50;
+#define ROTARY_ENCODER_A_PIN 26
+#define ROTARY_ENCODER_B_PIN 25
+#define ROTARY_ENCODER_BUTTON_PIN 27
+#define ROTARY_ENCODER_VCC_PIN -1 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
+
+// depending on your encoder - try 1,2 or 4 to get expected behaviour
+// #define ROTARY_ENCODER_STEPS 1
+// #define ROTARY_ENCODER_STEPS 2
+#define ROTARY_ENCODER_STEPS 4
+
+// instead of changing here, rather change numbers above
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+
+Adafruit_MCP4725 dac;
+
+const int ReceivePin = 36;
+
+void on_button_click();
+void handle_rotary_button();
+void rotary_loop(void *parameter);
+void IRAM_ATTR readEncoderISR();
+void getTime(void *param);
+
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin(115200);
+	while (!rtc.begin())
+	{
+		Serial.println("Couldn't find RTC");
+		Serial.flush();
+	}
+
+	if (rtc.lostPower())
+	{
+		Serial.println("RTC lost power, let's set the time!");
+		// When time needs to be set on a new device, or after a power loss, the
+		// following line sets the RTC to the date & time this sketch was compiled
+		rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+		// This line sets the RTC with an explicit date & time, for example to set
+		// January 21, 2014 at 3am you would call:
+		// rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+	}
+	IrReceiver.begin(ReceivePin, true, 13); // true: chế độ giám sát
+	while (!dac.begin(0x60))
+	{
+		Serial.println("DAC not found");
+	}
+	// we must initialize rotary encoder
+	rotaryEncoder.begin();
+	rotaryEncoder.setup(readEncoderISR);
+	bool circleValues = false;
+	rotaryEncoder.setBoundaries(0, 450, circleValues);
+	rotaryEncoder.setAcceleration(250); // or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
+	xTaskCreatePinnedToCore(rotary_loop, "Rotary Loop", CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL, 4, NULL, 0);
+	xTaskCreatePinnedToCore(getTime, "Get Time", CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL, 1, NULL, 1);
 }
 
 void loop()
 {
-	if (Serial.available() > 0) {
-    	String str = Serial.readString();
-    	str.trim();
-		DACData = str.toInt();
-	}
-	dacWrite(25, DACData);
-	Serial.println(ModifiedAnalogReadVolts(ADC_PIN, 10));
-}
-
-inline double linear_regression(double x)
-{
-	if((100 <= x) && (x <= 115))
+	if (IrReceiver.decode() > 0)
 	{
-		return (0.000821319907160756 * x + 0.15511300019077814);
-	}
-	else
-	{
-		return (0.0008199413026912705 * x + 0.16634694534577377);
-	}
-}
-
-double ModifiedAnalogReadVolts(uint8_t pin, uint16_t tolerance)
-{
-	uint32_t data[100] = {0};
-	for (int i = 0; i < 100; i++)
-	{
-		data[i] = analogRead(pin);
-	}
-	std::sort(data, data + 100);
-	int max = 0;
-	int left = -1;
-	int right = -1;
-	for (int i = 0; i < 10; i++)
-    {
-        uint32_t* it = std::upper_bound(data + i, data + 100, data[i] + tolerance);
-        if(it - data - i > max)
-        {
-            max = it - data - i;
-            left = i;
-            right = it - data - 1;
-        }
-    }
-	if(left == -1)
-	{
-		Serial.println("ADC Read Error");
-		return 0;
-	}
-	else
-	{
-		uint32_t sum = 0;
-		for (int i = left; i <= right; i++)
+		uint32_t dataRemote = IrReceiver.decodedIRData.decodedRawData; // Dữ liệu thô đã giải mã gán vào biến dataRemote
+		if (dataRemote > 0)
 		{
-			sum += data[i];
+			Serial.println("Dữ liệu nhận được : " + String(dataRemote));
+			Serial.println("Xin hãy nhấn nút !");
 		}
-		return linear_regression(double(sum / (right - left + 1)));
+		IrReceiver.resume(); // Cho phép nhận giá trị tiếp theo
 	}
+}
+
+void getTime(void* param)
+{
+	while(1)
+	{
+		DateTime now = rtc.now();
+
+		Serial.print(now.year(), DEC);
+		Serial.print('/');
+		Serial.print(now.month(), DEC);
+		Serial.print('/');
+		Serial.print(now.day(), DEC);
+		Serial.print(" (");
+		Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+		Serial.print(") ");
+		Serial.print(now.hour(), DEC);
+		Serial.print(':');
+		Serial.print(now.minute(), DEC);
+		Serial.print(':');
+		Serial.print(now.second(), DEC);
+		Serial.println();
+		
+		Serial.print("Temperature: ");
+		Serial.print(rtc.getTemperature());
+		Serial.println(" C");
+
+		Serial.println();
+		vTaskDelay(1000);
+	}
+}
+
+void on_button_click()
+{
+	Serial.print("button press ");
+	Serial.print(millis());
+	Serial.println(" milliseconds after restart");
+}
+
+void handle_rotary_button()
+{
+	static bool wasButtonDown = false;
+
+	bool isEncoderButtonDown = rotaryEncoder.isEncoderButtonDown();
+	// isEncoderButtonDown = !isEncoderButtonDown; //uncomment this line if your button is reversed
+
+	if (isEncoderButtonDown)
+	{
+		// we wait since button is still down
+		wasButtonDown = true;
+		return;
+	}
+
+	// button is up
+	if (wasButtonDown)
+	{
+		on_button_click();
+	}
+	wasButtonDown = false;
+}
+
+void rotary_loop(void *parameter)
+{
+	while (1)
+	{
+		// dont print anything unless value changed
+		if (rotaryEncoder.encoderChanged())
+		{
+			Serial.print("Value: ");
+			Serial.println(rotaryEncoder.readEncoder());
+			float preValue = (float(rotaryEncoder.readEncoder()) / 100) / (4.973) * 4095;
+			uint16_t value = uint16_t(preValue);
+			if (preValue - value >= 0.5)
+			{
+				value++;
+			}
+			Serial.println(value);
+			dac.setVoltage(value, false);
+		}
+		handle_rotary_button();
+		vTaskDelay(1);
+	}
+}
+
+void IRAM_ATTR readEncoderISR()
+{
+	rotaryEncoder.readEncoder_ISR();
 }
